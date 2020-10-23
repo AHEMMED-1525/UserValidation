426f266b75fcf14ccefee52453fac2c0edf225b8
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-10-23 13:25:01 +0530

    Added : Validation for First name using Regex pattern

--- UserValidation.java
+++ UserValidation.java
@@ -1,13 +1,22 @@
 package com.bridgelabz.uservalidations;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class UserValidation 
+class UserValidation 
 	{
-
-		public static void main(String[] args)
+		static final String FirstName = "^[A-Z]{1}[a-z]{2,}";
+		Scanner input=new Scanner(System.in);
+		
+		//Method for First name validation	
+		public void firstName()
 		{
-			UserRegistration uservalidation=new UserRegistration();
-			uservalidation.firstName();
+			System.out.println("Enter your First name : ");
+			String fname =input.next();
+			Pattern pattern = Pattern.compile(FirstName);
+			Matcher matcher = pattern.matcher(fname);
+			System.out.println("First name : " + fname +" "+ matcher.matches());
 		}
 	
 	}
