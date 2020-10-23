80c05cc4b3afc5d9ba86942856d01b876b31a515
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-10-23 13:51:39 +0530

    Added : Validation for Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,13 +1,12 @@
 package com.bridgelabz.uservalidations;
 
-
-
  public class UserRegistration {
-	
 		public static void main(String[] args)
 		{
 			UserValidation uservalidation = new UserValidation();
-			uservalidation.firstName();
+			 // first name validation
+				uservalidation.firstName();
+			 // last name validation
+				uservalidation.lastName();			
 		}
-
 }
--- UserValidation.java
+++ UserValidation.java
@@ -6,17 +6,27 @@ import java.util.regex.Pattern;
 
 class UserValidation 
 	{
-		static final String FirstName = "^[A-Z]{1}[a-z]{2,}";
-		Scanner input=new Scanner(System.in);
+		static final String Name_Pattern = "^[A-Z]{1}[a-z]{2,}";
+		Scanner input = new Scanner(System.in);
 		
 		//Method for First name validation	
 		public void firstName()
 		{
 			System.out.println("Enter your First name : ");
-			String fname =input.next();
-			Pattern pattern = Pattern.compile(FirstName);
+			String fname = input.nextLine();
+			Pattern pattern = Pattern.compile(Name_Pattern);
 			Matcher matcher = pattern.matcher(fname);
-			System.out.println("First name : " + fname +" "+ matcher.matches());
+			boolean match =  matcher.matches();
+			System.out.println("First name : " + fname + match);
+		}
+		// Method for Last Name Validation
+		public void lastName() {
+			System.out.println("Enter your Last Name : ");
+			String lname = input.nextLine();
+			Pattern pattern = Pattern.compile(Name_Pattern);
+			Matcher matcher = pattern.matcher(lname);
+			boolean match =  matcher.matches();
+			System.out.println("Last name : " +lname +" "+match);
 		}
 	
 	}
